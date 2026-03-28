c8f8d39b99c3b311325b6ac91a12f3329295ad1e
moouhida <moouhida@debian.myguest.virtualbox.org>
2026-03-28 11:32:48 +0100

    ff

---
+++ Makefile
@@ -0,0 +1,35 @@
+COMPOSE_FILE   =  srcs/docker-compose.yml
+
+DATA_PATH =  /home/moouhida/data
+
+
+all :
+	@mkdir  =p $(DATA_PATH)/mysql
+	@mkdir  =p $(DATA_PATH)/wordpress
+	@docker-compose  -f $(COMPOSE_FILE) up --build  -d
+ # -f  use  thespecif folder 
+ #up  start all  teh containers 
+ # --builds  =  rebuild images from dockerfiles
+ #-d  = backgroud  (detached mode)
+down :
+	@docker_compose -f  $(COMPOSE_FILE) down
+
+clean :
+	@docker-compose  -f  $(COMPOSE_FILE) down  --rmi  all
+
+fclean : clean
+	@rm  =rf  $(DATA_PATH)
+	@docker  system  prume  -af
+
+re : fclean all
+
+#show all the running containers
+status  :
+	@docker-compose -f  $(COMPOSE_FILE) ps
+
+#logs for debugging errrors
+
+logs:
+	$docker-compose -f  $(COMPOSE_FILE) logs
+
+.PHONY :  all  doen  clean  fclean  re  status  logs
---
+++ srcs/requirements/mariadb/tools/enrtrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+mysqld_safe &
+
+#wiat for  mariadb to  fully start /wihtout  it  next  command  whoule  run  before  mariadb  and  fail
+sleep 5
+
+#read password  from  secrets
+MYSQL_PASSWORD=$(cat /run/secrets/db_password)
+MYSQL_ROOT_PASSWORD =$(cat  /run/secrets/db_root_password)
+
+#opens mysql  session as root  user  /everythin until  the next  EOF  is sql commad to run
+mysql -u  root  <<EOF
+
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+
+CREATE USER IF NOT EXISTS '%{MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}' @ '%';
+
+ALTER  USER 'root'@'localhost' IDENTIFY BY '${MYSQL_ROOT_PASSWORD}';
+
+DELETE  FROM mysql.user WHERE User='';
+
+DELETE FROM mysql.user WHERE User='root'  AND NOT IN  ('localhost' , '127.0.0.1');
+
+ 
+FLUSH PRIVILEGES;
+EOF
+
+mysqladmin  -u  root  -p${MYSQL_ROOT_PASSWORD} shutdown
+
+exec mysqld_safe
