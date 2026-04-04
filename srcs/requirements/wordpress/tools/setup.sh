7779b124e795b2b76928798fafc034fe7bad2e5a
moouhidaa <mouaadouhida@gmail.com>
2026-04-04 07:26:44 +0000

    site uploaded ++++

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 #  wait for  mariadb  ro be  ready
+
 #docker-compose up starts all  the containers at  once so  mariadb  not  were be  ready ye
 until  mysqladmin ping -h  mariadb  --silent  2>/dev/null; do  sleep 1; done
+
+WP_ADMIN=$(cat 'NR=1{print $1}' /run/secrets/credentials |  cut -d= -f2)
+WP_ADMIN_PASSWORD=$(cat 'NR=2{print $1}' /run/secrets/credentials |  cut -d= -f2)
+WP_ADMIN_EMAIL=$(cat 'NR=3{print $1}' /run/secrets/credentials |  cut -d= -f2)
+WP_USER=$(cat  'NR=4{print $1}' /run/secrets/credentials | cut -d= -f2)
+WP_USER_PASSWORD=$(cat  'NR=5{print $1}' /run/secrets/credentials | cut -d= -f2)
+WP_PRESS_USER_EMAIL=$(cat 'NR=5{print $1}' /run/secrets/credentials | cut -d= -f2)
+
 
 MYSQL_PASSWORD=$(cat /run/secrets/db_password)
 
@@ -40,7 +49,7 @@ if [ ! -f /var/www/html/wp-config.php ];  then
         --role=author \
         --user_pass=$WP_PRESS_USER_PASSWORD \
         --allow-root
-
+    #allow wp-cli  run  as root  wihtout  any  problems
     wp config  set  WP_REDIS_HOST redis --allow-root
     wp config  set  WP_REDIS_PORT 6379 --raw --allow-root
     wp config set WP_CASH true --raw --allow-root
