9c3fcd9e2c33d6121d543dd8b1b89da3a698e66f
moouhidaa <mouaadouhida@gmail.com>
2026-04-08 11:55:51 +0100

    A little

--- srcs/requirements/bonus/backup/tools/backup.sh
+++ srcs/requirements/bonus/backup/tools/backup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+MYSQL_PASSWORD=$(cat /run/secrets/db_password)
+
 while  true
 do
     echo "Starting Backup (per 1h)...⏳"
-    mysqldump  -h  mariadb -u  $DB_SUER  -p $DB_PASSWORD $DB_NAME >  /home/moouhida/data/backup_mariadb.sql
+    mysqldump  -h  mariadb -u  $MYSQL_USER  -p$MYSQL_PASSWORD $MYSQL_DATABASE >  /home/moouhida/data/backup_mariadb.sql
     tar -czf /home/moouhida/data/db_wordpress.tar.gz  /var/www/html
     echo "Backup Complete with Success 👻"
     sleep 7200
