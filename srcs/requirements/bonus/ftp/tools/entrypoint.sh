e7018f04eca2541d04b597f6bb53f258205c4cc3
moouhida <moouhida@debian.myguest.virtualbox.org>
2026-04-16 08:36:55 +0100

    well done 100%

--- srcs/requirements/bonus/ftp/tools/entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/entrypoint.sh
@@ -5,7 +5,6 @@ FTP_USER=$(grep '^FTP_USER=' /run/secrets/credentials | cut  -d= -f2)
 FTP_PASS=$(grep '^FTP_PASS=' /run/secrets/credentials | cut  -d= -f2)
 
 id  -u $FTP_USER &>/dev/null || useradd  -m -d /var/www/html  $FTP_USER
-
 #then set  the  password  we  just  created
 echo  "$FTP_USER:$FTP_PASS" | chpasswd
 #chanege  the owner  fo those files  that  in that dir to ftp_user so he can upload 
